f5a93916f1e630705550ac10a04f11b5a200b7cc
XIIIX89 <114373945+XIIIX89@users.noreply.github.com>
2023-11-08 10:46:10 -0800

    Fleshed out remaining functions

--- javascript.js
+++ javascript.js
@@ -14,19 +14,32 @@ else if (computerWins > playerWins) {alert("You lose!");}
 else {alert("Draw");}
 
 function game() {
-    let userWins = 0;
-    let pcWins = 0;
-    let userHand = player();
-    let pcHand = pc();
-
-    let winner = compare(userHand, pcHand);
+    let userHand = player(); //gets the user's play from the player function
+    console.log(`Player: ${userHand}`);
+    let pcHand = pc(); //gets the computer's play from pc function
+    console.log(`Computer: ${pcHand}`);
+    let winner = compare(userHand, pcHand); //compares the user's and computer's hands and assigns the victor to the winner variable
     return winner;
 }
 
 function player() {
-
+    let play = prompt("Enter rock, paper, or scissors."); //gets player's play
+    return play.toLowerCase(); //returns the lowercase version of player input
 }
 
 function pc() {
-    
+    let play = Math.floor(Math.random() * 90); //sets play equal to an integer from 0 to 90
+    if (play >= 0 && play < 30) {return "rock";}
+    else if (play >=30 && play < 60) {return "paper";}
+    else if (play >= 60 && play < 90) {return "scissors";}
+}
+
+function compare(userHand, pcHand) {
+    if (userHand === pcHand) {return "draw";}
+    else if (userHand === "rock" && pcHand === "paper") {return "Computer";}
+    else if (userHand === "rock" && pcHand === "scissors") {return "Player"}
+    else if (userHand === "paper" && pcHand === "rock") {return "Player";}
+    else if (userHand === "paper" && pcHand === "scissors") {return "Computer";}
+    else if (userHand === "scissors" && pcHand === "rock") {return "Computer";}
+    else if (userHand === "scissors" && pcHand === "paper") {return "Player";}
 }
